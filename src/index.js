46729c6c81a3e6877ebe2e883853e1b3aa5a741a
HMoreira2020 <moreiraheather@gmail.com>
2020-09-16 14:08:49 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const newTask = document.getElementById('new-task-description');
+const form = document.getElementById('create-task-form');
+const tasksList = document.getElementById('tasks') 
+
+
+
+form.addEventListener('submit', (e) => {
+  alert('I was clicked!')
+  e.preventDefault(); 
+  tasksList.innerHTML += `<li> ${newTask.value}
+  <button data-action = "delete"> X </button> 
+  </li>`; // adds a delete button for each task created 
+  taskForm.reset(); // to reset form after submit so input isn't there after submit
+});
+
+
+tasksList.addEventListener('click', (e) => {
+  if (e.target.dataset.action === "delete") {
+    e.target.parentElement.remove();
+  }
+  // makes sure that the action, delete, of the function matches the data-action of the button and then deletes the parent element.
+  // e.target means the target of the event which is click  
+});
+
+
 });
